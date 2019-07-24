009cf2700d55c2bc21f68ce56f2f0e9a0a9a4c0d
Priscilla Firpo <priscillayf@gmail.com>
2019-07-24 01:43:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var a = 5
+var b = 10
+
+function add(a, b) {
+  return a + b 
+}
+
+var a = 5
+var b = 10
+
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function inc(n) {
+  return n + 1
+}
+function dec(n) {
+  return n - 1
+}
+function makeInt(n) {
+  return n 
+}
